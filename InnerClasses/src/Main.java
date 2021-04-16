2b2766793ec028e9e43b4e0b39ae4e1f73898c2e
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-04-16 18:39:10 +0100

    issue done. closes #5.

---
+++ InnerClasses/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Gearbox mcLaren = new Gearbox(6);
+        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+        Gearbox.Gear second = mcLaren.new Gear(2, 15.3);
+        Gearbox.Gear third = mcLaren.new Gear(2, 17.8);
+        System.out.println(first.driveSpeed(1000));
+    }
+}
