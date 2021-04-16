5637dbee35acc07e60e6f2104d06028107556876
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-04-16 18:24:51 +0100

    issue done. closes #4.

--- InterfacesChallenge/src/Main.java
+++ InterfacesChallenge/src/Main.java
@@ -35,8 +35,13 @@ public class Main {
         System.out.println(tim);
         tim.setWeapon("Stormbringer");
         saveObject(tim);
-        loadObject(tim);
+        //loadObject(tim);
         System.out.println(tim);
+
+        ISaveable werewolf = new Monster("Werewolf", 20, 40);
+        System.out.println(werewolf);
+        System.out.println(((Monster) werewolf).getStrength());
+        saveObject(werewolf);
 
 
     }
