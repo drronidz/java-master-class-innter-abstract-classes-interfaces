4ee10788d19748613dea89a74e122064ff8eac53
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-04-17 14:08:49 +0100

    issue done. closes #9.

--- Abstraction/src/Bird.java
+++ Abstraction/src/Bird.java
@@ -1,4 +1,4 @@
-public abstract class Bird extends Animal {
+public abstract class Bird extends Animal implements CanFly {
 
     public Bird(String name) {
         super(name);
@@ -14,5 +14,8 @@ public abstract class Bird extends Animal {
         System.out.println("Breathe in, breathe out, repeat");
     }
 
-    public abstract void fly();
+    @Override
+    public void fly() {
+        System.out.println(getName() + " is flapping its wings");
+    }
 }
--- Abstraction/src/Parrot.java
+++ Abstraction/src/Parrot.java
@@ -3,8 +3,4 @@ public class Parrot  extends Bird{
         super(name);
     }
 
-    @Override
-    public void fly() {
-        System.out.println("Flitting from branch to branch");
-    }
 }
